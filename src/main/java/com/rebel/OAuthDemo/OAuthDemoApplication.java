89553918df63c0463d790e8fffc07f9da4b4c0c5
Hiren <rebeldevmurari@gmail.com>
2023-08-08 16:49:37 +0100

    trying to implement OAuth2.0 function

--- src/main/java/com/rebel/OAuthDemo/OAuthDemoApplication.java
+++ src/main/java/com/rebel/OAuthDemo/OAuthDemoApplication.java
@@ -2,12 +2,24 @@ package com.rebel.OAuthDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
-public class OAuthDemoApplication {
+@RestController
+public class OAuthDemoApplication
+{
 
+	@GetMapping("/")
+	public String message(Principal principal)
+	{
+		return "Hi"+principal.getName()+"Welcome to the OAuth Demo";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OAuthDemoApplication.class, args);
+		System.out.println("App is running !!");
 	}
 
 }
